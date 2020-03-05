ab322f8b22c9ec44cad0c62bed2a92133d80b241
kima111 <kima111@gmail.com>
2020-03-04 17:24:23 -0800

    updated router and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Navbar from '../src/components/Navbar'
+import Main from '../src/components/Main'
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 export default function App() {
   return (
-    <div>
-      REMANR Boiler Template
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Main} />
+        </Switch>
+        REMANR Boiler Template
+      </div>
+    </Router>
   )
 }
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function index() {
     return (
         <div>
+            <nav>
+            <Link to="/">
+                Home
+            </Link>
+            </nav>
             
         </div>
     )
