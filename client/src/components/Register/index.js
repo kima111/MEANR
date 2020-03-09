783f6d88a70fd70d8bb689a2ecef37c8026abe38
kima111 <kima111@gmail.com>
2020-03-08 19:10:06 -0700

    updated register modal

--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -3,8 +3,11 @@ import API from '../../utils/API'
 
 export default function Register() {
 
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
 
     return (
         <div>
@@ -17,9 +20,15 @@ export default function Register() {
                         </button>
                     </div>
                     <div className="modal-body">
+                        <input className="form-control mr-sm-2" type="firstname" placeholder="First Name" aria-label="First Name" value={firstName} onChange={event => setFirstName(event.target.value)}/>
+                        <br />
+                        <input className="form-control mr-sm-2" type="lastname" placeholder="Last Name" aria-label="Last Name" value={lastName} onChange={event => setLastName(event.target.value)}/>
+                        <br />
                         <input className="form-control mr-sm-2" type="username" placeholder="Username" aria-label="Username" value={username} onChange={event => setUsername(event.target.value)}/>
                         <br />
                         <input className="form-control mr-sm-2" type="password" placeholder="Password" aria-label="Password" value={password} onChange={event => setPassword(event.target.value)}/>
+                        <br />
+                        <input className="form-control mr-sm-2" type="email" placeholder="Email" aria-label="Email" value={email} onChange={event => setEmail(event.target.value)}/>
                     </div>
                     <div className="modal-footer">
                         <button type="button" onClick = {(event) => {
