74f8853be01337b458ab11338e2cd78763cca1c5
kima111 <kima111@gmail.com>
2020-03-14 10:55:55 -0700

    starting protected routes

---
+++ client/src/components/UserProtectedRoutes/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,  
+  Route,
+  Redirect
+} from 'react-router-dom'
+import {useSelector} from 'react-redux';
+
+
+export default function ProtectedRoute({component: Component,loggedIn: LoggedIn, ...rest }) {
+
+  const loggedInState = useSelector(state => state.isLogged)
+ 
+  return (
+    <div>
+    <Router>
+    <Route {...rest} render={(props) => (
+        loggedInState === true
+        ? <Component {...props} />
+        : <Redirect to='/login' />
+    )} />
+    </Router>
+    </div>
+  )
+}
+
+
