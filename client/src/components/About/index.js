4a7b01ed44355b105941c5923c15ea2a61269f54
kima111 <kima111@gmail.com>
2020-03-14 11:04:40 -0700

    updated about page

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -3,7 +3,24 @@ import React from 'react'
 export default function index() {
     return (
         <div>
-            About Page
+            <h2>REMANR (Redux Express Mongo Axios Node React)</h2>
+            <h3>Overview</h3>
+            <p>This will be a full stack boiler template for Redux, Express, Mongo, Axios, Node, and React. There will be other packages included.</p>
+            <hr></hr>
+            <h3>Dependencies</h3>
+            <ul>
+                <li>Redux</li>
+                <li>Express</li>
+                <li>Mongo</li>
+                <li>Mongoose</li>
+                <li>Axios</li>
+                <li>React</li>
+                <li>React-Router-DOM</li>
+                <li>Nodemon</li>
+                <li>Concurrently</li>
+                <li>Passport</li>
+                <li>BcryptJS</li>
+            </ul>
         </div>
     )
 }
