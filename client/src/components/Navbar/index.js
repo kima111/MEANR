5f3e95b9536f5b1dc95022b8f8e1f0ec223c2d5e
kima111 <kima111@gmail.com>
2020-03-16 14:08:31 -0700

    adjusted navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -80,7 +80,7 @@ export default function Navbar() {
 
         <div>
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to = "/" className="navbar-brand">REMANR</Link>
+            <Link to = "/" className="navbar-brand">MEANR</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
