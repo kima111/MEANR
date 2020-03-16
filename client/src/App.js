7f078ff8b182b6cff22a1300719ab955b1c2803c
kima111 <kima111@gmail.com>
2020-03-16 15:45:59 -0700

    switched from redux to context api

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,21 @@ import React from 'react'
 import Layout from '../src/components/Layout'
 import Main from '../src/components/Main'
 import About from '../src/components/About'
+import {UserContext} from './UserContext'
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 export default function App() {
   return (
+    
     <Router>
       <div>
         <Layout>
         </Layout>
         <Switch>
+          <UserContext.Provider value="hello from context">
           <Route exact path="/" component={Main} />
           <Route exact path="/About" component={About} />
+          </UserContext.Provider>
         </Switch>
       </div>
     </Router>
--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {UserContext} from '../../UserContext';
 import { Link } from 'react-router-dom'
 
-export default function index() {
+export default function Main() {
+    const msg = useContext(UserContext);
     return (
         <div>
             <div className="jumbotron">
                 <h1 className="display-4">MEANR</h1>
                 <p className="lead"> (Mongo, Express, Axios, Node, React) This is a full stack boiler plate.</p>
                 <hr className="my-4" />
+                <div>{msg}</div>
                 <p>You can use this boiler plate to create full stack applications quickly</p>
                 <Link to="/About" className="btn btn-primary btn-lg" href="#" role="button">Learn more</Link>
             </div>
