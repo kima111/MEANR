7f43c03f5464f61ecc74d75025c8bbe632e87fe6
kima111 <kima111@gmail.com>
2020-03-16 14:07:47 -0700

    adjusted home page

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -5,8 +5,8 @@ export default function index() {
     return (
         <div>
             <div className="jumbotron">
-                <h1 className="display-4">REMANR </h1>
-                <p className="lead"> (Redux, Express, Mongo, Axios, Node, React) This is a full stack boiler plate.</p>
+                <h1 className="display-4">MEANR</h1>
+                <p className="lead"> (Mongo, Express, Axios, Node, React) This is a full stack boiler plate.</p>
                 <hr className="my-4" />
                 <p>You can use this boiler plate to create full stack applications quickly</p>
                 <Link to="/About" className="btn btn-primary btn-lg" href="#" role="button">Learn more</Link>
