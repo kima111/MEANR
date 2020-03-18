b5837476e212d59f48b76f0e6fbad511060cb636
kima111 <kima111@gmail.com>
2020-03-18 13:46:42 -0700

    updated welcome page to reflect username

--- client/src/components/Welcome/index.js
+++ client/src/components/Welcome/index.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Container } from 'react-bootstrap'
+import { UserContext } from '../../UserContext';
 
-export default function index() {
+export default function Welcome() {
+    
+    const {userInfo} = useContext(UserContext);
+
     return (
     <Container>
         <h1>Welcome to MEANR</h1>
-        <p>You have successfully logged in</p>
+        <p>You have successfully logged in as {userInfo.username}</p>
     </Container>
     )
 }
