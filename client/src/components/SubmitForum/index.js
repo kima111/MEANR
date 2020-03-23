00c636b698584ceaa6e25e25678993fd6cc0d8be
kima111 <kima111@gmail.com>
2020-03-23 13:25:50 -0700

    removed slateJS

--- client/src/components/SubmitForum/index.js
+++ client/src/components/SubmitForum/index.js
@@ -1,23 +1,70 @@
 import React, { useEffect, useMemo, useState } from "react";
-import { createEditor } from 'slate'
-import { Container } from 'react-bootstrap'
-import { Slate, Editable, withReact } from 'slate-react'
+
+import { Container, Form, Col, Button } from 'react-bootstrap'
+
 
 export default function SubmitForm() {
-    const editor = useMemo(() => withReact(createEditor()), [])
-    const [value, setValue] = useState([
-        {
-          type: 'paragraph',
-          children: [{ text: 'A line of text in a paragraph.' }],
-        },
-      ])
+
+    
     return (
         <div>
             <Container>
                 <h1>Submit Forum</h1>
-                <Slate editor={editor} value={value} onChange={value => setValue(value)}>
-                    <Editable />
-                </Slate>
+        
+                <Form>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="formGridEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Example textarea</Form.Label>
+                        <Form.Control as="textarea" rows="3" />
+                    </Form.Group>
+                    <Form.Group controlId="formGridAddress1">
+                        <Form.Label>Address</Form.Label>
+                        <Form.Control placeholder="1234 Main St" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formGridAddress2">
+                        <Form.Label>Address 2</Form.Label>
+                        <Form.Control placeholder="Apartment, studio, or floor" />
+                    </Form.Group>
+
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="formGridCity">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridState">
+                            <Form.Label>State</Form.Label>
+                            <Form.Control as="select" value="Choose...">
+                                <option>Choose...</option>
+                                <option>...</option>
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridZip">
+                            <Form.Label>Zip</Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+                    </Form.Row>
+
+                    <Form.Group id="formGridCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
             </Container>
         </div>
     )
