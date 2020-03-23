fbdf8e320a5b2ea78eda3ac1e5a4912443e7d66f
kima111 <kima111@gmail.com>
2020-03-23 16:28:55 -0700

    update on forum

--- client/src/components/Forum/index.js
+++ client/src/components/Forum/index.js
@@ -1,24 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import API from '../../utils/API'
 import { Container } from 'react-bootstrap'
 
 export default function () {
+    const [forumTitle, setForumTitle] = useState("")
+    const [forumText, setForumText] = useState("")
+    // const loadForum = event => {
+    //     event.preventDefault();
+    //     API.getForums().then(
+    //         function (response) {
+    //             setForumData(response.data[0].title)
+    //             console.log(response)
+    //         }
+    //     )
 
+    // }
 
-    const loadForum = event => {
-        event.preventDefault();
-        API.getForums().then(
-            function (response) {
-                console.log(response)
-            }
-        )
+    useEffect(() => {
+       console.log("loaded")
+       API.getForums().then(
+        function (response) {
+            setForumTitle(response.data[0].title)
+            setForumText(response.data[0].forumText)
+            console.log(response)
+        }
+    )
+    }, [])
 
-    }
     return (
     <div>
        <Container>
            <h1>Forum</h1>
-           <button onClick={loadForum}>test</button>
+           <h2>title: {forumTitle}</h2>
+           <p>text: {forumText}</p>
        </Container>
     </div>
     )
