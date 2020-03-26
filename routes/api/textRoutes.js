0c4f96a358963f85fbfb5d9fa642246e32ef5435
kima111 <kima111@gmail.com>
2020-03-25 17:17:48 -0700

    added process.env

--- routes/api/textRoutes.js
+++ routes/api/textRoutes.js
@@ -2,20 +2,21 @@ const twilio = require('twilio')
 const router = require("express").Router();
 const cors = require('cors')
 
-const accountSid = ''
-const authToken = ''
+const accountSid = process.env.ACCOUNT_SID
+const authToken = process.env.AUTH_TOK
+const pNumber = process.env.PNUMBER
 
 const client  = new twilio(accountSid, authToken);
 
 router.use(cors());
 
 router.post('/sendTextMessage', function(req, res){
-    console.log(req.body.text)
-
+    console.log(req.body.text) 
+    // console.log(process.env.ACCOUNT_SID)
     client.messages.create({
         body: req.body.text,
         to: req.body.recipient,
-        from: ''
+        from: pNumber
     })
     .then(
         res.json({message: "message successfully sent"})
