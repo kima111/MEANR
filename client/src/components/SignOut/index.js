79827b107aaf758ea2925825e9547a18d105406b
kima111 <kima111@gmail.com>
2020-04-03 11:39:17 -0700

    updated sign out page

--- client/src/components/SignOut/index.js
+++ client/src/components/SignOut/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Jumbotron, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 export default function index() {
     return (
-        <Container>
-            <p>You have successfully signed out</p>
-            <Link to="/">Home</Link>
-        </Container>
-            
+        <Jumbotron fluid>
+            <Container>
+                <h3>You have successfully signed out</h3>
+                <br />
+                <Button variant="outline-secondary" as={ Link } to="/">Return to Home</Button>
+            </Container>
+        </Jumbotron>
        
     )
 }
