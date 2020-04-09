4d086002c831276f1c2ae14518054dcdb3ffb516
kima111 <kima111@gmail.com>
2020-04-09 14:22:25 -0700

    pushing build

---
+++ client/build/precache-manifest.2fea22fbfcb8b2b5620eea57c2259d07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8f60c9ca77423937292e1365b203dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65430e73d28012839179",
+    "url": "/static/css/2.e5d14bd1.chunk.css"
+  },
+  {
+    "revision": "f1a764bcacdfb9a88f13",
+    "url": "/static/css/main.5827ca6e.chunk.css"
+  },
+  {
+    "revision": "65430e73d28012839179",
+    "url": "/static/js/2.536f96d1.chunk.js"
+  },
+  {
+    "revision": "9002c7cf47b3d4392aafe8b262aa7978",
+    "url": "/static/js/2.536f96d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a764bcacdfb9a88f13",
+    "url": "/static/js/main.255671ef.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
