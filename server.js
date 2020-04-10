413a6690c74f8d1c4fb5a611c1f48b0e12822b8f
kima111 <kima111@gmail.com>
2020-04-10 14:05:02 -0700

    update

--- server.js
+++ server.js
@@ -14,13 +14,14 @@ app.use(express.json());
 //   app.use(express.static("client/build"));
 // }
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
 }
+
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 // // Add routes, both API and view
 // app.use(routes);
 app.use(require("./routes/index"));
