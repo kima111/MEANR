85fb2edde3879ca4c7d5d13ad6b901fb9f81dfad
kima111 <kima111@gmail.com>
2020-04-13 09:43:28 -0700

    edited router

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,12 @@ const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
 
-const express = require("express");
-const app = express();
-
-
-
 // API Routes
 router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
-// router.use(function(req, res) {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
-app.get("/test", function(req, res){
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
-    console.log(req);
-})
 module.exports = router;
--- server.js
+++ server.js
@@ -24,7 +24,6 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
 
-
 app.get('*', (request, response) => {
 	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
