92ef0d99dc4f2f3948c06b87ca4ab2d8fc6c2cc1
kima111 <kima111@gmail.com>
2020-04-21 15:59:04 -0700

    updated about page

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -6,11 +6,13 @@ export default function index() {
         <div>
             <Jumbotron fluid>
                 <Container>
-                    <h1>MEANR (Mongo Express Axios Node React)</h1>
-                    <h2>Overview</h2>
+                    <h2>MEANR</h2> 
+                    <p style={{fontSize: "1.2em"}}>(Mongo Express Axios Node React)</p>
+                    <hr />
+                    <h3>Overview</h3>
                     <p>MEANR is a full stack boiler template for Mongo, Express, Axios, Node, and React intended to aide developers looking to get their app off the ground in a hurry with an easy to use framework for accomplishing common tasks, such as Role Configured Authentication, Payment, Emailing, Forum, and Text Messaging. There will be other packages included in later versions.</p>
                     <p>You can find the installation instructions and code here: <a href = "https://github.com/kima111/MEANR">https://github.com/kima111/MEANR</a></p>
-                    <h2>Features</h2>
+                    <h3>Features</h3>
                     <ul>
                         <li>Supports local authentication and registration</li>
                         <li>User and Administrator protected routes</li>
