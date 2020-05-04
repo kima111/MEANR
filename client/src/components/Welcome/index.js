4a8690eb572b98dc153e600bf6e969ed181b10e2
kima111 <kima111@gmail.com>
2020-05-03 20:30:59 -0700

    updated welcome page

--- client/src/components/Welcome/index.js
+++ client/src/components/Welcome/index.js
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react'
 import { Container, Jumbotron } from 'react-bootstrap'
 import { UserContext } from '../../UserContext';
+import welcomeImage from '../../assets/images/welcome.jpg'
+
 
 export default function Welcome() {
     
     const {userInfo} = useContext(UserContext);
 
     return (
-    <Jumbotron fluid>
-    <Container>
-        <h1>Welcome to MEANR</h1>
-        <p>You have successfully logged in as <b>{userInfo.username}</b></p>
+    <Jumbotron  style={{background: `url(${welcomeImage}) fixed center center`, backgroundSize: 'cover', height: "95vh", margin: "0 auto"}} fluid>
+    <Container style={{backgroundColor: 'rgba(0, 0, 0, 0.2)', borderRadius: '15px', padding: '50px'}}>
+        <h1 style={{color: "#ffffff"}}>Welcome to MEANR</h1>
+        <p style={{color: "#ffffff"}}>You have successfully logged in as <b>{userInfo.username}</b></p>
     </Container>
     </Jumbotron>
     )
