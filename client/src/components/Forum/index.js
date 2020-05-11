0d98e1bd26f2cb565f0612cbada2fe8861363a23
kima111 <kima111@gmail.com>
2020-05-11 14:36:01 -0700

    fixed infinite loop bug and begining to implement socket

--- client/src/components/Forum/index.js
+++ client/src/components/Forum/index.js
@@ -25,7 +25,7 @@ export default function () {
         return () => {
             
         }
-    }, [forumInfo])
+    }, [])
 
     return (
     <div>
--- server.js
+++ server.js
@@ -5,6 +5,44 @@ const routes = require('./routes');
 const app = express();
 const helmet = require('helmet');
 const PORT = process.env.PORT || 3001;
+const socketIO = require('socket.io');
+
+const http = require('http');
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+let interval;
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+    clearInterval(interval);
+  });
+});
+
+const getApiAndEmit = socket => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit('FromAPI', response);
+};
+
+
+// io.on('connection', (client) => {
+//   client.on('subscribeToTimer', (interval) => {
+//     console.log('client is subscribing to timer with interval ', interval);
+//     setInterval(() => {
+//       client.emit('timer', new Date());
+//     }, interval);
+//   });
+// });
+
+// io.listen(PORT);
 
 // constants for passport
 const passport = require('./passport');
@@ -79,6 +117,6 @@ app.get('*', (request, response) => {
 
 
 // Start the API server
-app.listen(PORT, function() {
+server.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
