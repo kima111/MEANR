1e241b03af093047752b99fa08be76565e0d5461
kima111 <kima111@gmail.com>
2020-05-14 11:31:38 -0700

    cleaned up submit forum

--- client/src/components/SubmitForum/index.js
+++ client/src/components/SubmitForum/index.js
@@ -1,21 +1,14 @@
 import React, { useState } from "react";
 import API from "../../utils/API"
 import { Container, Form, Button, Jumbotron } from 'react-bootstrap'
-import { useHistory } from 'react-router-dom';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import '../SubmitForum/index.css'
 import history from '../History';
-import socketIOClient from 'socket.io-client'
 
 export default function SubmitForm() {
     const [title, setTitle] = useState('');
     const [forumText, setForumText] = useState('');
-
-    const ENDPOINT = "http://localhost:3001";
-    const socket = socketIOClient(ENDPOINT);
-   
-
     const modules = { toolbar: [
         [{ 'header': [1, 2, false] }],
         ['bold', 'italic', 'underline','strike', 'blockquote', 'code-block', 'script'],
@@ -26,15 +19,11 @@ export default function SubmitForm() {
         ['link'],
         ['clean', 'formula', 'video']
       ],};
-
+      
     const submitForum = event => {
         event.preventDefault();
         const currentDate = Date(Date.now()).toString()
-        // socket.emit('postForum',{
-        //     title: title,
-        //     forumText: forumText,
-        //     date: currentDate
-        // })       
+  
     API.submitForum({
         title: title,
         forumText: forumText,
@@ -49,9 +38,7 @@ export default function SubmitForm() {
         <div>
             <Jumbotron fluid>
             <Container>
-                
                 <h1>Submit Forum</h1>
-        
                 <Form>
                     <Form.Group controlId="forumTitle">
                         <Form.Label>Title</Form.Label>
@@ -59,7 +46,6 @@ export default function SubmitForm() {
                     </Form.Group>
                     <ReactQuill theme="snow"className='ql-container' style={{backgroundColor:'#FFFFFF'}}  modules={modules} value={forumText} onChange={setForumText}/>
                     <br />
-
                     <Button to="/Forum" variant="primary" type="submit" onClick={submitForum}>
                         Submit
                     </Button>
